ef31819918a86a0315fce4a320368f8996ea7b90
CCG <43356892+CaptainCharlieGreen@users.noreply.github.com>
2019-05-17 17:27:21 -0700

    moving prefab items to strings

--- src/lambdaRuntime/createRuntime.js
+++ src/lambdaRuntime/createRuntime.js
@@ -7,7 +7,9 @@ const fs = require('fs');
 module.exports = async (host, targetFunctions) => {
   const runtimeConfig = createConfig(host, targetFunctions);
   const bundle = new ArchiveBuilder();
-  await bundle.copyDirectory(hostPrefab);
+  // await bundle.copyDirectory(hostPrefab);
+  bundle.addDataAsFile(Buffer.from(getStratHijack()), 'node_modules/strat/index.js');
+  bundle.addDataAsFile(Buffer.from(getRuntimeText()), 'stratRuntime.js');
   bundle.addDataAsFile(Buffer.from(JSON.stringify(runtimeConfig)), 'config.json');
   host.artifacts.forEach(artifact => {
     const data = getDataFrom(artifact);
@@ -44,4 +46,73 @@ function getDataFrom (artifact) {
     }
   }
   return artifact.data;
+}
+
+//copy this to the lambda as /node_modules/strat/index.js
+function getStratHijack () {
+  return `
+const strat = require('../../stratRuntime');
+const domo = strat.getMajordomo();
+module.exports = {
+  //this needs to return the majordomo
+  //without any closures because of stack traces
+  getResolver: function () {
+    return domo;
+  }
+};`;
+}
+
+//copy this to the lambda as /stratRuntime.js
+function getRuntimeText () {
+  return `
+//config will be placed inside the lambda
+const config = require('./config.json');
+const cat = require('util').promisify(require('fs').readFile);
+const domoFactory = require(config.onHost['Strat.majordomo']);
+var domo;
+module.exports = {
+  //this won't get called until we've created the domo on line 11
+  getMajordomo: function () {
+    return domo;
+  },
+  handler: async function (event, context, callback) {
+    domo = await domoFactory(invoke, config.hostName);
+    try {
+      callback(null, await domo.dispatch(event));
+    } catch (e) {
+      callback(e);
+    }
+  }
+};
+
+async function invoke (reference, event) {
+  const onHostFile = config.onHost[reference]
+  if (onHostFile !== undefined) {
+    if (config.resources[reference]) {
+      const fileData = await cat(onHostFile);
+      return (fileData || Buffer.from('')).toString();
+    }
+    return require(onHostFile)(event);
+  }
+  const service = (reference || '').split('.')[0];
+  if (service !== undefined) {
+    return await callLambda(config.ranged[service], event);
+  } else {
+    throw new Error(\`Cannot invoke reference \${reference}\`);
+  }
+}
+
+async function callLambda (functionName, event) {
+  const AWS = require('aws-sdk');
+  const lambda = new AWS.Lambda();
+  const invoke = require('util').promisify(lambda.invoke.bind(lambda));
+
+  const response = await invoke({
+    InvocationType: 'RequestResponse',
+    FunctionName: functionName,
+    Payload: Buffer.from(JSON.stringify(event))
+  });
+
+  return JSON.parse(response.Payload);
+}`;
 }
